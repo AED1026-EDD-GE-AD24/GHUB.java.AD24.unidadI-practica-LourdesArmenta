14f72dced6fae94f79bba8e7b3963058ffb50c54
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-08-26 17:59:30 +0000

    Actualizacion clase Rational

--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -1,11 +1,27 @@
 package miPrincipal;
 
+import org.junit.jupiter.engine.script.ScriptAccessor.SystemPropertyAccessor;
+
 public class Principal {
     public String getGreeting() {
         return "Hola Mundo!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new Principal().getGreeting());
+        //System.out.println(new Principal().getGreeting());
+        Rational r1 = new Rational(2,3);
+        Rational r2 = new Rational(5,3);
+        Rational r3 = r1.add(r1, r2);
+        System.out.println("Resulado de la suma = "+r3);
+        r3 = r1.mult(r1, r2);
+        System.out.println("Resultado del producto = "+r3);
+        Rational r5 = new Rational (2,4);
+        Rational r6 = new Rational (4,8);
+        System.out.print(r5+ " y "+r6+r5.equal(r5, r6));
+        
+
+
+
+
     }
 }
---
+++ miPrincipal/Rational.java
@@ -0,0 +1,57 @@
+package miPrincipal;
+
+public class Rational {
+    
+    private int r[] = new int[2];
+    
+    public Rational() {
+        r[0] = 1;
+        r[1]= 1;
+    }
+    
+    public Rational(int a, int b) {
+        this.r[0] = a;
+        this.r[1] = b;
+    }
+
+    public Rational add(Rational a, Rational b){
+        Rational x = new Rational();
+        x.setNumerador(a.getNumerador()*b.getDenominador()+a.getDenominador()*b.getNumerador());
+        x.setDenominador(a.getDenominador()*b.getDenominador());
+        return x;
+
+    }
+
+    public Rational mult(Rational a, Rational b){
+        Rational x = new Rational();
+        x.setNumerador(a.getNumerador()* b.getNumerador());
+        x.setDenominador(a.getDenominador()*b.getDenominador());
+        return x;
+    }
+
+    public boolean equal(Rational a, Rational b){
+        return (a.getDenominador()*b.getNumerador() == 
+               b.getDenominador()*a.getNumerador());
+    }
+
+    public int getNumerador(){
+        return r[0];
+    }
+    public int getDenominador(){
+        return r[1];
+    }
+    public void setNumerador(int a){
+        this.r[0]= a;
+    }
+    public void setDenominador(int b){
+        this.r[1]=b;
+    }
+
+    @Override
+    public String toString(){
+        return r[0]+"/"+r[1];
+        
+    }
+
+    
+}
