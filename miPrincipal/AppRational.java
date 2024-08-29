8fd2621c4b765e9047773b34ce456f0a07d0e46f
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-08-29 17:41:06 +0000

    Actualizacion Principal

--- miPrincipal/AppRational.java
+++ miPrincipal/AppRational.java
@@ -14,28 +14,53 @@ public class AppRational{
 
         Rational r1 = new Rational(a,b);
 
-        System.out.print("Introduce el numerador 1:");
+        System.out.print("Introduce el numerador 2:");
         a = consola.nextInt();
-        System.out.print("Introduce el denominador 1:");
+        System.out.print("Introduce el denominador 2:");
         b = consola.nextInt();
 
         Rational r2 = new Rational(a,b);
          
         // Rational r1 = new Rational(2,3);
         //Rational r2 = new Rational(5,3);
-         Rational r3 = r1.add(r1, r2);
+         Rational r3 = r1.add( r2);
          System.out.println("Resulado de la suma = "+r3);
-         r3 = r1.mult(r1, r2);
+         r3 = r1.mult(r2);
          System.out.println("Resultado del producto = "+r3);
 
-         
-         Rational r5 = new Rational (2,4);
-         Rational r6 = new Rational (4,8);
-         System.out.println(r5+ " y "+r6+" son iguales?"+r5.equal(r5, r6));
 
-         Rational r7 = new Rational(2,3);
-        Rational r8 = new Rational(5,3);
+        System.out.print("Introduce el numerador 2:");
+        a = consola.nextInt();
+        System.out.print("Introduce el denominador 2:");
+        b = consola.nextInt();
+
+        Rational r5 = new Rational(a,b);
+        Rational r6 = new Rational (a,b);
+
+        // Rational r5 = new Rational (2,4);
+         //Rational r6 = new Rational (4,8);
+        System.out.println(r5+ " y "+r6+" son iguales?"+r5.equal(r6));
+
+
+        System.out.print("Introduce el numerador 3:");
+        a = consola.nextInt();
+        System.out.print("Introduce el denominador 3:");
+        b = consola.nextInt();
+
+        Rational r7 = new Rational(a,b);
+
+        System.out.print("Introduce el numerador 3:");
+        a = consola.nextInt();
+
+        System.out.print("Introduce el denominador 3:");
+        b = consola.nextInt();
+
+        Rational r8 = new Rational(a,b);
         Rational r9 = r7.add(r8);
+
+        //Rational r7 = new Rational(2,3);
+        //Rational r8 = new Rational(5,3);
+        //Rational r9 = r7.add(r8);
         System.out.println("Resultado de la suma = "+r9);
         r9 = r7.mult(r8);
         System.out.println("Resultado del producto = "+r9);
