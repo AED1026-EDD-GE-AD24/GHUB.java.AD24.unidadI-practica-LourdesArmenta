95a6fdbc8de9c7fd4fb91d58027ef0f871e95cd6
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-08-29 17:21:22 +0000

    Actualizacion Rational

--- miPrincipal/AppRational.java
+++ miPrincipal/AppRational.java
@@ -1,16 +1,34 @@
 package miPrincipal;
+import java.util.Scanner;
 public class AppRational{
     public static void menu(){
+        Scanner consola = new Scanner(System.in);
         System.out.println("\n======================================");
         System.out.println("Operaciones con Racionales ");
         System.out.println("======================================");
+
+        System.out.print("Introduce el numerador 1:");
+        int a = consola.nextInt();
+        System.out.print("Introduce el denominador 1:");
+        int b = consola.nextInt();
+
+        Rational r1 = new Rational(a,b);
+
+        System.out.print("Introduce el numerador 1:");
+        a = consola.nextInt();
+        System.out.print("Introduce el denominador 1:");
+        b = consola.nextInt();
+
+        Rational r2 = new Rational(a,b);
          
-         Rational r1 = new Rational(2,3);
-         Rational r2 = new Rational(5,3);
+        // Rational r1 = new Rational(2,3);
+        //Rational r2 = new Rational(5,3);
          Rational r3 = r1.add(r1, r2);
          System.out.println("Resulado de la suma = "+r3);
          r3 = r1.mult(r1, r2);
          System.out.println("Resultado del producto = "+r3);
+
+         
          Rational r5 = new Rational (2,4);
          Rational r6 = new Rational (4,8);
          System.out.println(r5+ " y "+r6+" son iguales?"+r5.equal(r5, r6));
--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -12,24 +12,30 @@ public class Principal {
         do{
             System.out.println("*******************************************");
             System.out.println("          Tipos de Datos Abstractos ");
-            System.out.println("***********************************++******");
+            System.out.println("*******************************************");
 
             System.out.println(" 1) Rational");
             System.out.println(" 2) Matriz");
-            System.out.println(" 3) Conjunto");
+            System.out.println(" 3) Cadena");
+            System.out.println(" 4) Numerote");
+            System.out.println(" 5) Conjunto");
             System.out.println("                       0) Salir");
 
             System.out.print("Seleccione una opcion (0 para salir ): ");
             opc = consola.nextInt();
             switch(opc){
                 case 1:
-                    AppRational.menu();
-                    break;
+                   AppRational.menu();
+                   break;
                 case 2:
                    
                 case 3:
-                    AppConjunto.menu();
-                    break;
+                   
+                case 4:
+                case 5:
+                AppConjunto.menu();
+                   break;
+
                 case 0:
                     System.out.println("Adios");
                     break;
@@ -39,6 +45,19 @@ public class Principal {
 
 
         } while (opc !=0);
+
+       
+
+       
+
+
+
+
+
+
+        
+
+
 
 
     }
